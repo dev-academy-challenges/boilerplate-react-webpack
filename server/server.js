c1bc4d4ae3350c949b281b97515f0fea59b37166
Rich Churcher <rich.churcher@gmail.com>
2016-09-26 16:44:35 +1300

    Improve webpack --watch behaviour
     * Moves bundle files and index.html to ./public
     * Background processes get killed when parent process receives
       SIGINT, so we can take advantage of this to get webpack
       --watch working with one initial server load

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ var PORT = process.env.PORT || 3000
 var app = express()
 
 app.use(bodyParser.json())
-app.use(express.static(path.join(__dirname, '/../client')))
+app.use(express.static(path.join(__dirname, '../public')))
 
 app.listen(PORT, function () {
   console.log('Listening on port', PORT)
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,14 @@
 module.exports = {
   entry: './client/index.js',
   output: {
-    path: './client',
+    path: './public',
     filename: 'bundle.js'
   },
   module: {
     loaders: [{
       test: /\.jsx?$/,
       loader: 'babel-loader',
-      exclude: 'node_modules'
+      exclude: /node_modules/
     }]
   },
   resolve: {
