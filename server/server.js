4059ec0c8ae4bded9ec3315afe9f67bac83eadcc
Don Smith <locksmithdon@gmail.com>
2017-01-11 00:16:02 +1300

    refactor server.js

--- server/server.js
+++ server/server.js
@@ -2,12 +2,9 @@ var path = require('path')
 var express = require('express')
 var bodyParser = require('body-parser')
 
-var PORT = process.env.PORT || 3000
-var app = express()
+var server = express()
 
-app.use(bodyParser.json())
-app.use(express.static(path.join(__dirname, '../public')))
+server.use(bodyParser.json())
+server.use(express.static(path.join(__dirname, '../public')))
 
-app.listen(PORT, function () {
-  console.log('Listening on port', PORT)
-})
+module.exports = server
