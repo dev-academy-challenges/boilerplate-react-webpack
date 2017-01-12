2b3f920d6890b3f284540642ec58e38ef1bfc270
Rich Churcher <rich.churcher@gmail.com>
2017-01-13 11:06:50 +1300

    No need for explicit create

--- test/index.test.js
+++ test/index.test.js
@@ -6,7 +6,7 @@ import App from '../client/components/App'
 
 test('<App />', t => {
   const expected = 'React development has begun!'
-  const wrapper = shallow(React.createElement(App))
-  t.deepEqual(wrapper.text(), expected)
+  const wrapper = shallow(<App />)
+  t.equal(wrapper.text(), expected)
   t.end()
 })
