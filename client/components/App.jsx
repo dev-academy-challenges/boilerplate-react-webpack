a308324e7c21e009a9365eace0fb8ac24973a719
Don Smith <locksmithdon@gmail.com>
2017-03-13 11:31:39 +1300

    convert to functional component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
-export default React.createClass({
-  render() {
-    return (
-      <h1>React development has begun!</h1>
-    )
-  }
-})
+const App = () => {
+  return (
+    <h1>React development has begun!</h1>
+  )
+}
+
+export default App
+
