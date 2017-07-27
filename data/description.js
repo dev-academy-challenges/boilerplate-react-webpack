33e84868b61f4ac4c56c78b0a39b5414802e2447
Usha <usha.macdonald@gmail.com>
2017-07-28 10:46:52 +1200

    added data file

---
+++ data/description.js
@@ -0,0 +1,14 @@
+[
+  {
+    name: 'Smelly Feet Ragdoll',
+    description: '/dfjadlkfgalsdhfkjadh'
+  },
+  {
+    name: 'Bad Breath Burmese',
+    description: '/dfjadlkfgalsdhfkjadh'
+  },
+  {
+    name: 'Pointy Ear Persian',
+    description: '/dfjadlkfgalsdhfkjadh'
+  }
+]
