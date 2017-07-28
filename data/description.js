5382f8069a1b59bd20f57e11dfc78f1168675055
Usha <usha.macdonald@gmail.com>
2017-07-28 12:01:52 +1200

    added script

--- data/description.js
+++ data/description.js
@@ -1,14 +1,19 @@
 [
   {
     name: 'Smelly Feet Ragdoll',
-    description: '/dfjadlkfgalsdhfkjadh'
+    description: ''
   },
   {
     name: 'Bad Breath Burmese',
-    description: '/dfjadlkfgalsdhfkjadh'
+    description: 'You\'re going to want to keep your distance from the Bad Breath Burmese. One whiff and youll be
+    chucking up fur balls all week. These particular pairs of whiskers are known for their ability to
   },
   {
     name: 'Pointy Ear Persian',
     description: '/dfjadlkfgalsdhfkjadh'
   }
 ]
+
+hard coding himalayan
+These particular pairs of whiskers will always catch you out when you least expect it. The values youre using change
+and bam - the hard coding himalayan has you blocked. 
