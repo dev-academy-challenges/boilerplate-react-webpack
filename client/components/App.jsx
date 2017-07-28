4719f9136a2f128b0d3f6d87292e6e941c5ac57c
Chris Chou <chhschou@gmail.com>
2017-07-28 15:29:37 +1200

    made display of results and form conditional

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,21 +1,29 @@
 import React from 'react'
-
 import Results from './Results'
-
 import Form from "./Form"
-r
 
-const App = () => {
-  let people = []
+export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { people: [] }
+  }
 
-  return (
-    <div>
-      <h1>Mythical teams</h1
-        <Results people={people}/>
-     <Form />
-  </div>
+  setPeople(people) {
+    this.setState({
+      people: people
+    })
+  }
 
-  )
+  render() {
+    return (
+      <div>
+        <h1>Sorting Cats</h1>
+        {this.state.people.length > 0 ?
+          <Results people={people} /> :
+          <Form onPeopleCompleted={this.setPeople} />
+        }
+      </div>
+
+    )
+  }
 }
-
-export default App
