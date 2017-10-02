ab1a558bf87a49c015afe21047452520c218f303
Don Smith <don@antiantonym.com>
2017-10-03 08:56:35 +1300

    consistency with other boilerplates

--- server/server.js
+++ server/server.js
@@ -5,6 +5,6 @@ var bodyParser = require('body-parser')
 var server = express()
 
 server.use(bodyParser.json())
-server.use(express.static(path.join(__dirname, '../public')))
+server.use(express.static(path.join(__dirname, './public')))
 
 module.exports = server
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
+const path = require('path')
+
 module.exports = {
   entry: './client/index.js',
   output: {
-    path: __dirname + '/public',
+    path: path.join(__dirname, 'server/public'),
     filename: 'bundle.js'
   },
   module: {
