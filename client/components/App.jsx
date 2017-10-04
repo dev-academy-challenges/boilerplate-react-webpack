bf1e0d6762c206e7c49f064be89dca5792a240d2
Your Name <you@example.com>
2017-10-04 17:59:09 +1300

    first commit

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 
 const App = () => {
   return (
-    <h1>React development has begun!</h1>
+    <div>
+    <h1>"Welcome to the Shared Lunch App..."</h1>
+
+    <figure className="image is-128x128">
+      <img src="https://www.thesun.co.uk/wp-content/uploads/2017/01/gettyimages-185071735.jpg?strip=all&w=960" />
+    </figure>
+  </div>
   )
 }
 
 export default App
-
