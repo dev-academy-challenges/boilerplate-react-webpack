7d9dcc9c4d72422c03a874e6b4e688dbcdbce754
dazyang <dazyang.contact@gmail.com>
2017-10-05 18:11:07 +1300

    data js

--- server/index.js
+++ server/index.js
@@ -3,6 +3,6 @@ var server = require('./server')
 var PORT = process.env.PORT || 3000
 
 server.listen(PORT, function () {
-  console.log('Listening on port', PORT)
+  console.log('Team Selkie in the House', PORT)
 })
 
