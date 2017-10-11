5f038a243d6985b7f1c306d0a9ff5a0ea51df702
Rajal Givesh Kumar <RajalKumar@Prems-MacBook-Air.local>
2017-10-12 09:19:40 +1300

    home page sorted

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import {HashRouter as Router, Route, Link} from 'react-router-dom'
+
+import Home from './Home.jsx'
 
 const App = () => {
   return (
-    <h1>React development has begun!</h1>
+    <Router>
+    <div className='App'>
+    <h1>SK imports & exporting</h1>
+    <div className='container'>
+    <Route path='/' component={Home} />
+    </div>
+    </div>
+    </Router>
   )
 }
 
 export default App
-
---
+++ client/components/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <h1>Home page</h1>
+  )
+}
+
+export default Home
