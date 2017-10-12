f800e9c7b51bbc9c5d41c7dca10642b8e1507bf5
richardjohns <you@example.com>
2017-10-12 22:41:35 +1300

    added dependencies

--- server/db.js
+++ server/db.js
@@ -4,90 +4,108 @@ module.exports = {
 }
 
 var vids = [{
-    id: 1,
-    vidTitle: 'Joe\s talk',
-    vidUrl: '',
-    vidPicUrl: '',
-    vidDesc: 'Acme Inc.',
-    commentGood: [
-        {
-            comment: 'That was amazing!'
-        }
-    ],
-    commentBad: [
-        {
-            comment: 'That was terrible!'
-        }
-    ],
-    knowledge: 2,
-    humour: 4,
-    useofBoard: 6,
-    flow: 8,
-    topic: 7
-}, {
-    id: 2,
-    vidTitle: 'Joe\s talk',
-    vidUrl: '',
-    vidPicUrl: '',
-    vidDesc: 'Acme Inc.',
-    commentGood: [
-        {
-            comment: 'That was amazing!'
-        }
-    ],
-    commentBad: [
-        {
-            comment: 'That was terrible!'
-        }
-    ],
-    knowledge: 2,
-    humour: 4,
-    useofBoard: 6,
-    flow: 8,
-    topic: 7
-}, {
-    id: 3,
-    vidTitle: 'Joe\s talk',
-    vidUrl: '',
-    vidPicUrl: '',
-    vidDesc: 'Acme Inc.',
-    commentGood: [
-        {
-            comment: 'That was amazing!'
-        }
-    ],
-    commentBad: [
-        {
-            comment: 'That was terrible!'
-        }
-    ],
-    knowledge: 2,
-    humour: 4,
-    useofBoard: 6,
-    flow: 8,
-    topic: 7
-}, {
-    id: 4,
-    vidTitle: 'Joe\s talk',
-    vidUrl: '',
-    vidPicUrl: '',
-    vidDesc: 'Acme Inc.',
-    commentGood: [
-        {
-            comment: 'That was amazing!'
-        }
-    ],
-    commentBad: [
-        {
-            comment: 'That was terrible!'
-        }
-    ],
-    knowledge: 2,
-    humour: 4,
-    useofBoard: 6,
-    flow: 8,
-    topic: 7
-}]
+        id: 1,
+        vidTitle: 'Joe\s talk',
+        vidUrl: '',
+        vidPicUrl: '',
+        vidDesc: 'Acme Inc.',
+        commentGood: [{
+                comment: 'That was amazing!'
+            },
+            {
+                comment: 'That was double-amazing!'
+            }
+        ],
+        commentBad: [{
+                comment: 'That was terrible!'
+            },
+            {
+                comment: 'That was beyond terrible!'
+            }
+        ],
+        knowledge: 2,
+        humour: 4,
+        useofBoard: 6,
+        flow: 8,
+        topic: 7
+    }, {
+        id: 2,
+        vidTitle: 'Joe\s talk',
+        vidUrl: '',
+        vidPicUrl: '',
+        vidDesc: 'Acme Inc.',
+        commentGood: [{
+                comment: 'That was amazing!'
+            },
+            {
+                comment: 'That was double-amazing!'
+            }
+        ],
+        commentBad: [{
+                comment: 'That was terrible!'
+            },
+            {
+                comment: 'That was beyond terrible!'
+            }
+        ],
+        knowledge: 2,
+        humour: 4,
+        useofBoard: 6,
+        flow: 8,
+        topic: 7
+    },
+    {
+        id: 3,
+        vidTitle: 'Joe\s talk',
+        vidUrl: '',
+        vidPicUrl: '',
+        vidDesc: 'Acme Inc.',
+        commentGood: [{
+                comment: 'That was amazing!'
+            },
+            {
+                comment: 'That was double-amazing!'
+            }
+        ],
+        commentBad: [{
+                comment: 'That was terrible!'
+            },
+            {
+                comment: 'That was beyond terrible!'
+            }
+        ],
+        knowledge: 2,
+        humour: 4,
+        useofBoard: 6,
+        flow: 8,
+        topic: 7
+    }, {
+        id: 4,
+        vidTitle: 'Joe\s talk',
+        vidUrl: '',
+        vidPicUrl: '',
+        vidDesc: 'Acme Inc.',
+        commentGood: [{
+                comment: 'That was amazing!'
+            },
+            {
+                comment: 'That was double-amazing!'
+            }
+        ],
+        commentBad: [{
+                comment: 'That was terrible!'
+            },
+            {
+                comment: 'That was beyond terrible!'
+            }
+        ],
+        knowledge: 2,
+        humour: 4,
+        useofBoard: 6,
+        flow: 8,
+        topic: 7
+    }
+]
 
 function getVids() {
     return vids
