950efe490842f60d14b167c2f3a44d810a56861b
Octavia <octaviapalmer@hotmail.com>
2017-10-13 11:00:39 +1300

    we have a blueprint of our comment map

--- client/components/Feedbacks.jsx
+++ client/components/Feedbacks.jsx
@@ -2,31 +2,41 @@ import React from 'react'
 
 import { HashRouter as Router, Route } from 'react-router-dom'
 
-const Feedbacks = () => {
+const Feedbacks = (props) => {
   return (
     <div className="feedbacks">
       <div className="neg-feedback">
         <div id="negs">...Constructive feedbacks...</div>
+        <div>
+          <div>{props.commentBad.map(comment =>
+            <p>{comment}</p>
+          )}
+          </div>
 
+        </div>
 
         <input type="text" name="neg-feed" placeholder="Give your constructive feedback here!"></input>
         <input type="submit" value="Submit my comment"></input>
       </div>
       <div className="pos-feedback">
         <div id="pos">...Positive feedbacks...
-          
+          <div>
+            <div>{props.commentGood.map(comment =>
+              <p>{comment}</p>
+            )}
+            </div>
+
+          </div>
+
+          <input type="text" name="pos-feed" placeholder="Unleash your praise here!"></input>
+          <input type="submit" value="Submit my comment"></input>
         </div>
-
-
-        <input type="text" name="pos-feed" placeholder="Unleash your praise here!"></input>
-        <input type="submit" value="Submit my comment"></input>
       </div>
     </div>
   )
 }
 
 export default Feedbacks
-
 
 // class Main_con extends React.Component {
 //   constructor(props) {
@@ -40,13 +50,11 @@ export default Feedbacks
 //         <div className="neg-feedback">
 //           <div id="negs">Constructive feedbacks</div>
 
-
 //           <input type="text" name="neg-feed" placeholder="Give your constructive feedback here!"></input>
 //           <input type="submit" value="Submit my comment"></input>
 //         </div>
 //         <div className="pos-feedback">
 //           <div id="pos">Positive feedbacks</div>
-
 
 //           <input type="text" name="pos-feed" placeholder="Unleash your praise here!"></input>
 //           <input type="submit" value="Submit my comment"></input>
