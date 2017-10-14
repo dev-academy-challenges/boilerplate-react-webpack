20ec239cc89e1c8c80e81e169570be636f540d4c
Rajal Kumar <rajalkumar13@hotmail.com>
2017-10-14 16:15:50 +1300

    updated and installed superagent

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
-var path = require('path')
-var express = require('express')
-var bodyParser = require('body-parser')
+const path = require('path')
+const express = require('express')
+const bodyParser = require('body-parser')
 
-var server = express()
+const server = express()
 
 server.use(bodyParser.json())
 server.use(express.static(path.join(__dirname, './public')))
+
+servr.use('/api/v1/car_info', require('./routes'))
 
 module.exports = server
