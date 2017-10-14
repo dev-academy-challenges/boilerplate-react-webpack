869833050c03eca3bbf5338cec8900f81934f7e5
Rajal Kumar <rajalkumar13@hotmail.com>
2017-10-14 15:59:03 +1300

    client-api & Car component added

---
+++ client/components/Car.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Switch, Route, Link } from 'react-router-dom'
+
+
+class Car extends React.Component {
+
+  render() {
+    return (
+      <div className='car'>
+        <h2></h2>
+
+      </div>
+    )
+  }
+}
+
+export default Car
