bcdb068a86f93a2f7ca086ec6d4d4de97e064380
Skye <skye.simpso@gmail.com>
2017-10-15 18:01:53 +1300

    back to working format

--- client/components/Lunch.jsx
+++ client/components/Lunch.jsx
@@ -2,86 +2,40 @@ import React from 'react'
 
 import data from '../../data/lunch'
 
-// const lunches = Lunches(data)[0]
+const Lunch = () => {
 
-// function Lunches(props) {
-//   return (
-//     <div className='lunches'>
-//       {props.lunches.map(lunch => {
-//         return <lunch key={lunch.id} lunch={lunch} />
-//       })}
-//     </div>
-//   )
-// }
+    function values(data) {
+        return Object.keys(data).map(prop => {
+            return data[prop]
+        })
+    }
+    const lunches = values(data)[0]
 
+    return (
+        <div>
+            <h1>The Lunch Menu</h1>
+            <ul>
+                {lunches.map(lunch => {
+                    return <li key={lunch.id}>
+                        <ul>
+                            <li>{lunch.name}</li>
+                            <li>{lunch.description}</li>
+                            <ul>
+                                {lunch.ingredients.map(ingredient => {
+                                    return <li key={ingredient.type}>
+                                        {ingredient.type}
+                                    </li>
+                                })}
+                            </ul>
+                        </ul>
+                    </li>
+                })}
+            </ul>
+        </div>
+    )
+}
 
-
-
-
-// class Lunch extends React.Compnent {
-
-//     constructor (props) {
-//       super(props)
-//       this.state = {
-//         lunches: [ ]
-//       }
-//       this.renderLunches = this.renderLunches.bind(this)
-//     }
-     
-//     ComponentDidmount () {
-//       getLunches(this.renderLunches)
-//     }
-
-//     renderWidgets (err, lunches) {
-//       this.setState({
-//         lunches: lunches
-//       })
-//     }
-
-//     render () {
-//       return (
-//         <div>
-//           <h1>Lunches FTW!</h1>
-//           <LunchList lunches={this.state.lunches} />
-//         </div>
-//       )
-//     }
-//   }
-
-
-
-//   function values (data) {
-//     return Object.keys(data).map(prop => {
-//       return data[prop]
-//     })
-//   }
-//   const lunches = values(data)[0]
-
-//   return (
-//     <div className="hero is-fullheight is-danger is-bold has-text-centered">
-
-//       <h1>The Lunch Menu</h1>
-//       <ul>
-//         {lunches.map(lunch => {
-//           return <li key={lunch.id}>
-//             <ul>
-//               <li>{lunch.name}</li>
-//               <li>{lunch.description}</li>
-//             </ul>
-//           </li>
-//         })}
-//       </ul>
-//     </div>
-//   )
-// }
-
-
-
-
-
-
-
-export default Lunches
+export default Lunch
 
 
 // in This component I'm showing three options for lunches  -  I'd like to use to be able to select one of these lunches to see the ingrediants and then click on the ingrediants they would like to bring and have it move to another list 
--- data/lunch.js
+++ data/lunch.js
@@ -1,17 +1,17 @@
-
-const Lunches = [
+const meals = {
+  lunches: [
     {
       id: 1,
       name: 'Mexican',
       description: 'For a tasty meal that will satisfy the whole team, whip up a plate of these delicious Mexican Nachos. Juicy tomatoes, onions and jalapeños with Mexican-inspired herbs. They will not disappoint!',
       ingredients: [
-        {type: '300 g lean beef mince'},
-        {type: '1 medium onion, diced'},
-        {type: '375 g pouch Watties Mexican Creations Nachos Simmer Sauce'},
-        {type: '400 g can Watties Red Kidney Beans in Springwater, drained'},
-        {type: '175 g bag nacho chips'},
-        {type: '½ cup grated cheese'},
-        {type: '½ cup sour cream (optional)'}
+        { type: '300 g lean beef mince' },
+        { type: '1 medium onion, diced' },
+        { type: '375 g pouch Watties Mexican Creations Nachos Simmer Sauce' },
+        { type: '400 g can Watties Red Kidney Beans in Springwater, drained' },
+        { type: '175 g bag nacho chips' },
+        { type: '½ cup grated cheese' },
+        { type: '½ cup sour cream (optional)' }
       ]
     },
     {
@@ -19,17 +19,17 @@ const Lunches = [
       name: 'Vegetarian',
       description: 'Inspired by Hare Krishna $3 lunches in Dunedin, this special dish will be tasty and also gives you your daily dose of vegetables. Delicious!',
       ingredients: [
-        {type: '3 tablespoons vegetable oil'},
-        {type: '1 teaspoons fennel seeds'},
-        {type: '1 large onion, finely chopped'},
-        {type: '½ teaspoons ground turmeric'},
-        {type: 'Chilli powder to taste'},
-        {type: '4 tablespoons tomato purée'},
-        {type: '250g cauliflower florets'},
-        {type: 'type:  green capsicum, cored, deseeded and cubed'},
-        {type: '4 tomatoes, quartered'},
-        {type: '600ml plant based milk'},
-        {type: 'Small bunch of coriander, finely chopped (include some stalks, which have lots of flavour)'}
+        { type: '3 tablespoons vegetable oil' },
+        { type: '1 teaspoons fennel seeds' },
+        { type: '1 large onion, finely chopped' },
+        { type: '½ teaspoons ground turmeric' },
+        { type: 'Chilli powder to taste' },
+        { type: '4 tablespoons tomato purée' },
+        { type: '250g cauliflower florets' },
+        { type: 'type:  green capsicum, cored, deseeded and cubed' },
+        { type: '4 tomatoes, quartered' },
+        { type: '600ml plant based milk' },
+        { type: 'Small bunch of coriander, finely chopped (include some stalks, which have lots of flavour)' },
       ]
     },
 
@@ -38,16 +38,16 @@ const Lunches = [
       name: 'The Classic',
       description: 'The classic workplace shared lunch featuring all of your favourite treats. Sausage rolls, party pies, cheerios, T-sauce, chips, tim tams, lollies and fizzy drinks. Ride the sugar high all afternoon!',
       ingredients: [
-        {type: 'Sausage Rolls'},
-        {type: 'Party Pies'},
-        {type: 'Cheerios'},
-        {type: 'T-sauce'},
-        {type: 'chips'},
-        {type: 'tim tams'},
-        {type: 'lollies'},
-        {type: 'fizzy drink'}
+        { type: 'Sausage Rolls' },
+        { type: 'Party Pies' },
+        { type: 'Cheerios' },
+        { type: 'T-sauce' },
+        { type: 'chips' },
+        { type: 'tim tams' },
+        { type: 'lollies' },
+        { type: 'fizzy drink' },
       ]
     }
   ]
-
-export default Lunches
+}
+export default meals
