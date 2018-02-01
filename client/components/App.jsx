2b17542c2cd0928bc26a8fa46ead863eff3be166
Julia Niall <jnmareike@gmail.com>
2018-02-02 10:34:13 +1300

    name input working and showing

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
 
-const App = () => {
-  return (
-    <h1>Sorting Hat</h1>
-  )
+import Form from './Form.jsx'
+import NameList from './NameList.jsx'
+
+class App extends React.Component {
+  constructor () {
+    super()
+    this.state = {
+      names: []
+    }
+    this.addName = this.addName.bind(this)
+  }
+
+  addName (name) {
+    this.setState({
+      names: [...this.state.names, name]
+    })
+  }
+
+  render () {
+    return (
+      <div>
+        <h1>Sorting Hat</h1>
+        <Form addName={this.addName} />
+        <NameList names={this.state.names} />
+      </div>
+    )
+  }
 }
 
 export default App
-
