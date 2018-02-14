cc0e15d45f3e252e0f357ffaf6c61c5bad7110ce
Don Smith <don@antiantonym.com>
2018-02-14 18:35:31 +1300

    improve test setup

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,14 +1,10 @@
 import React from 'react'
-import Enzyme, {shallow} from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
+import {shallow} from 'enzyme'
 
 import App from '../client/components/App'
-
-Enzyme.configure({adapter: new Adapter()})
 
 test('<App />', () => {
   const expected = 'React development has begun!'
   const wrapper = shallow(<App />)
   expect(wrapper.text()).toBe(expected)
 })
-
