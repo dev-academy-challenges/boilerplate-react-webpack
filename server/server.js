864c3508fc26d4e96596aaff7d90e659dee5b7df
Don Smith <don@antiantonym.com>
2018-03-26 12:08:39 +1300

    remove body-parser

--- server/server.js
+++ server/server.js
@@ -1,11 +1,9 @@
 const path = require('path')
 const express = require('express')
-const bodyParser = require('body-parser')
 
 const server = express()
 
-server.use(bodyParser.json())
+server.use(express.json())
 server.use(express.static(path.join(__dirname, './public')))
 
 module.exports = server
-
--- webpack.config.js
+++ webpack.config.js
@@ -18,4 +18,3 @@ module.exports = {
   },
   devtool: 'source-map'
 }
-
