b82940d20df11ffa82a6f2eafedea65aa781ee2a
anthony-xiao <juniorxiao01@gmail.com>
2018-06-22 09:31:14 +1200

    fixed nav

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {HashRouter as Router} from 'react-router-dom'
+import {HashRouter as Router, Route} from 'react-router-dom'
 import Nav from './Nav'
 import Rules from './Rules'
 
