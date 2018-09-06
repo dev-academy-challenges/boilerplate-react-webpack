3bfef4652f817a3a08369274ab8047e4bfce8d66
Amy Han <40330174+amy-han@users.noreply.github.com>
2018-09-06 18:03:32 +1200

    set up

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 
-const App = () => {
+import {HashRouter as Router, Route} from 'react-router-dom'
+
+import Home from './Home'
+
+const App = props => {
   return (
-    <h1>React development has begun!</h1>
+    <Router>
+      <div>
+        <Route path='/' component={Home} />
+      </div>
+    </Router>
+
   )
 }
-
 export default App
 
---
+++ client/components/Map.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Map = () => {
+  return (
+    <div id="mapid"></div>
+  )
+}
+
+export default Map
