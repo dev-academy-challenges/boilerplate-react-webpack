c03cfad5bde635a94a1733c2b49cf6d27fab232b
nepcam <cameron.nepe@gmail.com>
2018-09-08 18:34:19 +1200

    boiling up

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 const path = require('path')
 const express = require('express')
 
+
 const server = express()
+
 
 server.use(express.json())
 server.use(express.static(path.join(__dirname, './public')))
