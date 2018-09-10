cf8abf02447111a921044846c898918c0db7adb6
Amy Han <40330174+amy-han@users.noreply.github.com>
2018-09-10 13:58:01 +1200

    update and simplified home.jsx for map

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,12 +3,18 @@ import React from 'react'
 import {HashRouter as Router, Route} from 'react-router-dom'
 
 import Home from './Home'
+import Events from './Events'
+import Gallery from './Gallery'
+import Artists from './Artists'
 
 const App = props => {
   return (
     <Router>
       <div>
         <Route path='/' component={Home} />
+        <Route path='/Events' component={Events} />
+        <Route path='/Gallery' component={Gallery} />
+        <Route path='/Artists' component={Artists} />
       </div>
     </Router>
   )
--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -5,15 +5,11 @@ import Menu from './Menu'
 import ShowMap from './ShowMap'
 
 const Home = () => {
-  const coords = {
-    lat: -36.8471567,
-    lng: 174.7649395
-  }
   return (
     <div>
       <Header />
       <Menu />
-      <ShowMap coords={coords} />
+      <ShowMap />
     </div>
   )
 }
