555bd3cae764536a61cea5a164064c472815b540
Amy Han <40330174+amy-han@users.noreply.github.com>
2018-09-12 20:04:21 +1200

    working on marker popups

--- client/components/Galleries.jsx
+++ client/components/Galleries.jsx
@@ -1,20 +1,20 @@
-// import React from 'react'
-// import Header from './Header'
-// import Menu from './Menu'
-// import information from '../../data/information'
+import React from 'react'
+import Header from './Header'
+import Menu from './Menu'
+import information from '../../data/information'
 
-// export default class Galleries extends React.Component {
-//   render () {
-//     const GalleryInfo = ({match}) => {
-//       const {gallery, name} = match.params
-//       const galleryInfo = information[galleries].filter(item => item.name === name)[0]
-//     return (
-//       <div>
-//         <Header />
-//         <Menu />
-//         <h3>Galleries</h3>
-//         <p>{galleryInfo.description}</p>
-//       </div>
-//     )
-//   }
-// }
+export default class Galleries extends React.Component {
+  render () {
+    const GalleryInfo = ({match}) => {
+      const {gallery, name} = match.params
+      const galleryInfo = information[galleries].filter(item => item.name === name)[0]
+    return (
+      <div>
+        <Header />
+        <Menu />
+        <h3>Galleries</h3>
+        <p>{GalleryInfo}</p>
+      </div>
+    )
+  }
+}
