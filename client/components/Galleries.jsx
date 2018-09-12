bc2b4fc5f31541b7b1ded7f63d39daa4eeb996dd
Amy Han <40330174+amy-han@users.noreply.github.com>
2018-09-12 15:51:32 +1200

    marker is working!

--- client/components/Galleries.jsx
+++ client/components/Galleries.jsx
@@ -3,16 +3,16 @@ import Header from './Header'
 import Menu from './Menu'
 import GalleriesList from '../../data/galleries'
 
-export default class Galleries extends React.Component {
-  
-  render() {
-    return (
-      <div>
-        <Header />
-        <Menu />
-        <h3>Galleries</h3>
-        
-      </div>
-    )
-  }
+export default function Galleries(props) {
+  const galleryInfo = GalleriesList.map((item, iterator) => {
+    return <li key={iterator}></li>
+  })
+  return (
+    <div>
+      <Header />
+      <Menu />
+      <h3>Galleries</h3>
+      <ul> {galleryInfo} </ul>
+    </div>
+  )
 }
