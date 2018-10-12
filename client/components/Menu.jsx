ef64cbfb8b7ca11c4162df6b19b39a8e31b1c30e
Amy Han <40330174+amy-han@users.noreply.github.com>
2018-10-12 23:56:40 +1300

    add semantic UI for css

--- client/components/Menu.jsx
+++ client/components/Menu.jsx
@@ -2,19 +2,13 @@ import React from 'react'
 
 import { Link } from 'react-router-dom'
 
-export default class Menu extends React.Component {
-  render() {
-    return (
-      <div className='menu'>
-        <ul>
-          <div className="btn-group-wrap" role="group" aria-label="Basic example">
-            <Link to={'/'}><button type="button" >Home</button></Link>
-            <Link to={'/exhibitions'}><button className="btn btn-flat btn-lg">Exhibitions</button></Link>
-            <Link to={'/galleries'}><button className="btn btn-flat btn-lg">Galleries</button></Link>
-            <Link to={'/artists'}><button className="btn btn-flat btn-lg">Artists</button></Link>
-            </div>
-        </ul>
-      </div>
-    )
-  }
+export default function Menu (props) {
+  return (
+    <div class="ui secondary menu">
+      <a class="item"><Link to={'/'}>Home</Link></a>
+      <a class="item"><Link to={'/exhibitions'}>Exhibitions</Link></a>
+      <a class="item"><Link to={'/galleries'}>Galleries</Link></a>
+      <a class="item"><Link to={'/artists'}>Artists</Link></a>
+    </div>
+  )
 }
