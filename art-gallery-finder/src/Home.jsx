5456d0bca879095695a725c6343e65f8d28f9e0e
Amy Han <40330174+amy-han@users.noreply.github.com>
2018-10-31 19:50:55 +1300

    app and home component

---
+++ art-gallery-finder/src/Home.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Home extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Art Gallery Finder</h1>
+                </div>
+        )
+    }
+}
