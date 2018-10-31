310377897e4973e2ee4cca302690dff5f94c970d
Amy Han <40330174+amy-han@users.noreply.github.com>
2018-10-31 23:13:43 +1300

    added react-leaflet

--- art-gallery-finder/src/ShowMap.jsx
+++ art-gallery-finder/src/ShowMap.jsx
@@ -1,10 +1,21 @@
 import React, { Component } from 'react'
+import { render } from 'react-dom'
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
 
 export default class ShowMap extends Component {
+    const position = [51.505, -0.09]
     render() {
         return (
             <div>
-                <p>Map</p>
+                  <Map center={position} zoom={13}>
+    <TileLayer
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+    />
+    <Marker position={position}>
+      <Popup>A pretty CSS3 popup.<br />Easily customizable.</Popup>
+    </Marker>
+  </Map>
                 </div>
         )
     }
