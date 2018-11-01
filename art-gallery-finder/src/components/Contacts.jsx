5b8f516d69e04ea4defec03c7ca2344866a58117
Amy Han <40330174+amy-han@users.noreply.github.com>
2018-11-01 23:21:02 +1300

    contact component

--- art-gallery-finder/src/components/Contacts.jsx
+++ art-gallery-finder/src/components/Contacts.jsx
@@ -4,15 +4,16 @@ export default class Contacts extends Component {
     constructor (props) {
         super (props)
         this.state =
-        
+
     }
     render() {
         return (
             <div>
-                <ul>
-                    <li><a href="/"><button>Home</button></a></li>
-                    <li><a href="/contacts"><button>Contacts</button></a></li>
-                </ul>
+                <form>
+                    <input
+                    placeholder = "name"
+                    value = "name" />
+                    </form>
             </div>
         )
     }
