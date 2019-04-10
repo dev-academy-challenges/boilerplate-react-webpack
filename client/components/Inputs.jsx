b27a36bb9baa4eb97a76624575dcc3a76f3a7712
Paul Renwick <prenwick@gmail.com>
2019-04-11 11:59:25 +1200

    Working on reduce

--- client/components/Inputs.jsx
+++ client/components/Inputs.jsx
@@ -6,18 +6,24 @@ class Inputs extends React.Component {
   render () {
     return (
       <React.Fragment>
-        <button onClick={session.push('Incorrect')} className='button'>Incorrect</button>
-        <button onClick={session.push('Prompt')} className='button'>Prompt</button>
-        <button onClick={session.push('Correct')} className='button'>Correct</button>
+        <button onClick={() => session.push('0')}
+          className='button'>Incorrect</button>
+        <button onClick={() => session.push('0')}
+          className='button'>Prompt</button>
+        <button onClick={() => session.push('1')}
+          className='button'>Correct</button>
         <br></br><br></br>
-        <button onClick={console.log(session)} className='button'>Done</button>
+        <button onClick={() => console.log(session.reduce(
+          (accumulator, currentValue) => accumulator + currentValue, 0
+        )}
+        className='button'>Done</button>
       </React.Fragment>
     )
   }
 }
 
 export default Inputs
-//Need to have it so that + pushes a correct value to array
+// Need to have it so that + pushes a correct value to array
 // - and P push incorrect and prompt
 
 // Done button takes the array and reduced it into a % correct
@@ -25,3 +31,6 @@ export default Inputs
 
 // Ideally I then want to take a copy of the array that was made
 // perhaps a map, and then have that displayed if the user wants to
+
+// Going to start so that Incorret and prompt only push 0 and correct pushes 1
+// for sake of ease with reduce.
