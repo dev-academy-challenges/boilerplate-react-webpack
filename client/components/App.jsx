b5ce9747ad3ebe5b126404d4047a3acbd2ee26b3
ihakapink <ihakapink@gmail.com>
2019-04-16 14:04:08 +1200

    updated app.jsx and profiles.jsx

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
 
+export default class App extends React.Component {
+  constructor (props) {
+      super(props)
+
+      this.state = {
+          error: null,
+          buttons: [],
+          activeProfile: null,
+          detailsVisible: false,
+          addButtonVisible: false
+           }
+
+      this.refreshList = this.refreshList.bind(this)
+      this.showDetails = this.showDetailsList.bind(this)
+      this.hideDetails = this.hideDetails.bind(this)
+      this.renderProfiles = this.renderButtons.bind(this)
+      this.showAddProfiles = this.showAddProfiles.bind(this)
+  }
+  
+    componentDidMount () {
+      this.refreshList()
+    }
+
+}
+
 const App = () => {
   return (
-    <h1>React development has begun!</h1>
+    <h1>Draft Project!</h1>
   )
 }
 
