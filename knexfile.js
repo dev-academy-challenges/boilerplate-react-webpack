8d795d1693780d28ddb9a96b5b11df7806fffbf6
Paul Renwick <prenwick@gmail.com>
2019-04-22 08:50:20 +1200

    Finally connecting

---
+++ knexfile.js
@@ -0,0 +1,36 @@
+module.exports = {
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './dev.sqlite3'
+    },
+    useNullAsDefault: true
+  },
+
+  test: {
+    client: 'sqlite3',
+    connection: {
+      filename: ':memory:'
+    },
+    seeds: {
+      directory: './tests/seeds'
+    },
+    useNullAsDefault: true
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user: 'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+}
