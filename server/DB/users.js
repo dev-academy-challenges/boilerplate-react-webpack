1fd1bf568158ac29b8ce061aaf9a139355b59202
ihakapink <ihakapink@gmail.com>
2019-05-22 09:38:16 +1200

    added usersjs functions in db for ss

---
+++ server/DB/users.js
@@ -0,0 +1,31 @@
+const environment = process.env.NODE_ENV || 'development'
+const config = require('../db/knexfile')[environment]
+const connection = require('knex')(config)
+const hash = require('../auth/hash')
+
+module.exports = {
+  createUser,
+  userExists
+}
+
+function createUser (username, password, db = connection) {
+  return userExists(username, db)
+    .then(exists => {
+      if (exists) {
+        return Promise.reject(new Error('User exists'))
+      }
+    })
+    .then(() => hash.generate(password))
+    .then(passwordHash => {
+      return db('users').insert({ username, hash: passwordHash })
+    })
+}
+
+function userExists (username, db = connection) {
+  return db('users')
+    .count('id as n')
+    .where('username', username)
+    .then(count => {
+      return count[0].n > 0
+    })
+}
