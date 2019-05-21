0ef32c91e5b1ca0ef6589e6f1bac90589cc329e9
ihakapink <ihakapink@gmail.com>
2019-05-21 14:02:42 +1200

    restart project

--- server/index.js
+++
@@ -1,8 +0,0 @@
-const server = require('./server')
-
-const port = process.env.PORT || 3000
-
-server.listen(port, function () {
-  // eslint-disable-next-line no-console
-  console.log('Listening on port', port)
-})
--- server/server.js
+++
@@ -1,9 +0,0 @@
-const path = require('path')
-const express = require('express')
-
-const server = express()
-
-server.use(express.json())
-server.use(express.static(path.join(__dirname, './public')))
-
-module.exports = server
