5df11c5ecc2d57f9d64193f8cb87289d27c8f71d
ihakapink <ihakapink@gmail.com>
2019-05-21 14:31:46 +1200

    indexjs added

---
+++ server/index.js
@@ -0,0 +1,9 @@
+require('dotenv').config()
+const server = require('./server')
+
+const port = process.env.PORT || 3000
+
+server.listen(port, function () {
+  // eslint-disable-next-line no-console
+  console.log('Listening on port', port)
+})
