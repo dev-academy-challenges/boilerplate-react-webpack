9875bb7cd735211c3ea320621a683fa2db4c1f70
ihakapink <ihakapink@gmail.com>
2019-05-24 22:26:53 +1200

    added db function

--- tests/server/db/users.test.js
+++ tests/server/db/users.test.js
@@ -1,19 +1,33 @@
 const env = require('./test-environment')
 const db = require('../../../server/DB/users')
 
-let testDb = null 
+let testDb = null
 
 beforeAll(() => {
-    testDb = env.getTestDb()
-    return env.initialise(testDb)
+  testDb = env.getTestDb()
+  return env.initialise(testDb)
 })
 
 afterEach(() => env.cleanup(testDb))
 
 test('getUserById returns a user given their id', () => {
-    return db.getUserById(1, testDb)
+  return db.getUserById(1, testDb)
     .then(user => {
-        expect(user.username).toBe('')
+      expect(user.username).toBe('')
     })
     .catch(err => expect(err).toBeNull())
+})
+
+test('createUser creates a new user', () => {
+  const username = 'iamnew'
+  const validateNewUser = newIds => {
+    const id = newIds[0]
+    return db.getUserById(id, testDb)
+      .then(user => {
+        expect(user.username).toBe(username)
+      })
+  }
+  return db.createUser(username, '', testDb)
+    .then(validateNewUser)
+    .catch(err => expect(err).toBeNull)
 })
