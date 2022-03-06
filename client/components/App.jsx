623545f28d7bc8bc9ec7356fa084e86242c35722
rohan-fowlerharper <rohan.fowlerharper@gmail.com>
2022-03-07 10:20:58 +1300

    add basic error handling

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -4,20 +4,31 @@ import { getGreeting } from '../apiClient'
 const App = () => {
   const [greeting, setGreeting] = useState('')
   const [count, setCount] = useState(0)
+  const [isError, setIsError] = useState(false)
 
   useEffect(() => {
-    /* eslint-disable promise/catch-or-return, promise/always-return */
-    getGreeting().then((greeting) => {
-      console.log(greeting)
-      setGreeting(greeting)
-    })
-    /* eslint-enable */
+    getGreeting()
+      .then((greeting) => {
+        console.log(greeting)
+        setGreeting(greeting)
+        setIsError(false)
+        return null
+      })
+      .catch((err) => {
+        console.log(err)
+        setIsError(true)
+      })
   }, [count])
 
   return (
     <>
       {count}
       <h1>{greeting}</h1>
+      {isError && (
+        <p style={{ color: 'red' }}>
+          There was an error retrieving the greeting.
+        </p>
+      )}
       <button onClick={() => setCount(count + 1)}>Click</button>
     </>
   )
